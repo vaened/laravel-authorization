4ebc45378b7dcc4cb44f7786c96eb73c69367e33
enea dhack <enea.so@live.com>
2018-03-14 22:53:25 -0500

    Merge tables and config classes

--- src/Support/Config.php
+++ src/Support/Config.php
@@ -5,8 +5,46 @@
 
 namespace Enea\Authorization\Support;
 
+use Enea\Authorization\Models\Permission;
+use Enea\Authorization\Models\Role;
+
 class Config
 {
+    public static function permissionModel(): string
+    {
+        return self::get('models.permission', Permission::class);
+    }
+
+    public static function roleModel(): string
+    {
+        return self::get('models.role', Role::class);
+    }
+
+    public static function userPermissionTableName(): string
+    {
+        return self::get('tables.user_permissions', 'user_permissions');
+    }
+
+    public static function userRoleTableName(): string
+    {
+        return self::get('tables.user_roles', 'user_roles');
+    }
+
+    public static function permissionTableName(): string
+    {
+        return self::get('tables.permission', 'permissions');
+    }
+
+    public static function roleTableName(): string
+    {
+        return self::get('tables.role', 'roles');
+    }
+
+    public static function rolePermissionTableName(): string
+    {
+        return self::get('tables.role_has_many_permissions', 'role_permissions');
+    }
+
     public static function getDriver(): string
     {
         return self::get('driver', 'database');
@@ -17,7 +55,7 @@ class Config
         return self::get('listeners.unauthorized-owner-logger', true);
     }
 
-    public static function get(string $key, string $default = null): string
+    private static function get(string $key, string $default = null): string
     {
         return config("authorization.{$key}", $default);
     }
