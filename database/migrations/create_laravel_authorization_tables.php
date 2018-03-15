3ffabea88dfeabec8aaaa45e50766fda39cbb023
enea dhack <enea.so@live.com>
2018-03-14 22:57:54 -0500

    Refactoring migrations

--- database/migrations/create_laravel_authorization_tables.php
+++ database/migrations/create_laravel_authorization_tables.php
@@ -1,5 +1,6 @@
 <?php
 
+use Enea\Authorization\Support\Config;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,19 +9,17 @@ class CreateLaravelAuthorizationTables extends Migration
 {
     public function up(): void
     {
-        $tables = config('authorization.tables');
+        Schema::create(Config::roleTableName(), $this->getGrantableStructure());
+        Schema::create(Config::permissionTableName(), $this->getGrantableStructure());
+        Schema::create(Config::userRoleTableName(), $this->getAuthorizationsStructure(Config::roleTableName()));
+        Schema::create(Config::userPermissionTableName(), $this->getAuthorizationsStructure(Config::permissionTableName()));
 
-        Schema::create($tables['role'], $this->getGrantableStructure());
-        Schema::create($tables['permission'], $this->getGrantableStructure());
-        Schema::create($tables['user_roles'], $this->getAuthorizationsStructure(str_singular($tables['role'])));
-        Schema::create($tables['user_permissions'], $this->getAuthorizationsStructure(str_singular($tables['permission'])));
-
-        Schema::create($tables['role_has_many_permissions'], function (Blueprint $table) use ($tables) {
+        Schema::create(Config::rolePermissionTableName(), function (Blueprint $table): void {
             $table->unsignedInteger('role_id');
-            $table->foreign('role_id')->references('id')->on($tables['role']);
+            $table->foreign('role_id')->references('id')->on(Config::roleTableName());
 
             $table->unsignedInteger('permission_id');
-            $table->foreign('permission_id')->references('id')->on($tables['permission']);
+            $table->foreign('permission_id')->references('id')->on(Config::permissionTableName());
 
             $table->primary(['role_id', 'permission_id']);
 
@@ -30,17 +29,16 @@ class CreateLaravelAuthorizationTables extends Migration
 
     public function down(): void
     {
-        $tables = config('authorization.tables');
-
-        Schema::dropIfExists($tables['role_has_many_permissions']);
-        Schema::dropIfExists($tables['permission']);
-        Schema::dropIfExists($tables['role']);
-        Schema::dropIfExists($tables['authorizable_group']);
+        Schema::dropIfExists(Config::rolePermissionTableName());
+        Schema::dropIfExists(Config::userPermissionTableName());
+        Schema::dropIfExists(Config::userRoleTableName());
+        Schema::dropIfExists(Config::permissionTableName());
+        Schema::dropIfExists(Config::roleTableName());
     }
 
-    private function getGrantableStructure()
+    private function getGrantableStructure(): Closure
     {
-        return function (Blueprint $table) {
+        return function (Blueprint $table): void {
             $table->increments('id');
             $table->string('secret_name', 60)->unique();
             $table->string('display_name', 60);
@@ -49,17 +47,18 @@ class CreateLaravelAuthorizationTables extends Migration
         };
     }
 
-    private function getAuthorizationsStructure($tableName)
+    private function getAuthorizationsStructure($tableName): Closure
     {
-        return function (Blueprint $table) use ($tableName) {
+        return function (Blueprint $table) use ($tableName): void {
             $table->increments('id');
 
             $table->unsignedInteger('authorizable_id');
             $table->string('authorizable_type', 100);
             $table->index(['authorizable_id', 'authorizable_type']);
 
-            $table->unsignedInteger("{$tableName}_id");
-            $table->foreign("{$tableName}_id")->references('id')->on(str_plural($tableName));
+            $name = str_singular($tableName);
+            $table->unsignedInteger("{$name}_id");
+            $table->foreign("{$name}_id")->references('id')->on($tableName);
 
             $table->timestamps();
         };
