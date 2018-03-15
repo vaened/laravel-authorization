18c0e77c68b9901fc1d38af9a1b32bc1083a9d81
enea dhack <enea.so@live.com>
2018-03-14 21:34:46 -0500

    Refactor support classes
    - refactor Config
    - refactor Tables

--- src/Support/Config.php
+++ src/Support/Config.php
@@ -9,15 +9,15 @@ class Config
 {
     public static function getDriver(): string
     {
-        return self::getConfig('driver', 'database');
+        return self::get('driver', 'database');
     }
 
     public static function listenUnauthorizedOwnerEventForLogger(): bool
     {
-        return self::getConfig('listeners.unauthorized-owner-logger', true);
+        return self::get('listeners.unauthorized-owner-logger', true);
     }
 
-    private static function getConfig(string $key, string $default = null): string
+    public static function get(string $key, string $default = null): string
     {
         return config("authorization.{$key}", $default);
     }
--- src/Support/Tables.php
+++ src/Support/Tables.php
@@ -12,36 +12,36 @@ class Tables
 {
     public static function userPermissionModel(): string
     {
-        return config('authorization.tables.user_permissions', 'user_permissions');
+        return Config::get('tables.user_permissions', 'user_permissions');;
     }
 
     public static function userRoleModel(): string
     {
-        return config('authorization.tables.user_roles', 'user_roles');
+        return Config::get('tables.user_roles', 'user_roles');
     }
 
     public static function permissionModel(): string
     {
-        return config('authorization.models.permission', Permission::class);
+        return Config::get('models.permission', Permission::class);
     }
 
     public static function roleModel(): string
     {
-        return config('authorization.models.role', Role::class);
+        return Config::get('models.role', Role::class);
     }
 
     public static function permissionName(): string
     {
-        return config('authorization.tables.permission', 'permissions');
+        return Config::get('tables.permission', 'permissions');
     }
 
     public static function roleName(): string
     {
-        return config('authorization.tables.role', 'roles');
+        return Config::get('tables.role', 'roles');
     }
 
     public static function rolePermissionName(): string
     {
-        return config('authorization.tables.role_has_many_permissions', 'role_permissions');
+        return Config::get('tables.role_has_many_permissions', 'role_permissions');
     }
 }
