741023989761f1063944d30b9dcf75c2e107920f
enea dhack <enea.so@live.com>
2018-03-19 20:15:24 -0500

    Add database driver tests

---
+++ tests/Drivers/Database/DatabaseTestCase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created on 14/03/18 by enea dhack.
+ */
+
+namespace Enea\Authorization\Tests\Drivers\Database;
+
+use Enea\Authorization\Support\Drivers;
+use Enea\Authorization\Tests\Drivers\DriverTestCase;
+
+class DatabaseTestCase extends DriverTestCase
+{
+    protected function getDriver(): string
+    {
+        return Drivers::DATABASE;
+    }
+}
