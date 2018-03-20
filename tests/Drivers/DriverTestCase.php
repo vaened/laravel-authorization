a6b62f44112aa8a60581d0d5e655ba169cb07c80
enea dhack <enea.so@live.com>
2018-03-19 19:59:57 -0500

    Add base DriverTestCase

---
+++ tests/Drivers/DriverTestCase.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created on 19/03/18 by enea dhack.
+ */
+
+namespace Enea\Authorization\Tests\Drivers;
+
+use Enea\Authorization\Tests\TestCase;
+
+abstract class DriverTestCase extends TestCase
+{
+    abstract protected function getDriver(): string;
+
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+        $app->make('config')->set('authorization.driver', $this->getDriver());
+    }
+}
