b90cb400cabf1cc9b8a10ac826e9b159467dcfbe
enea dhack <enea.so@live.com>
2018-03-20 19:08:34 -0500

    Add test for roles in AuthorizableTest

--- tests/Models/AuthorizableTest.php
+++ tests/Models/AuthorizableTest.php
@@ -6,11 +6,12 @@
 namespace Enea\Authorization\Tests\Models;
 
 use Enea\Authorization\Contracts\PermissionContract;
+use Enea\Authorization\Contracts\RoleContract;
 use Enea\Authorization\Tests\TestCase;
 
 class AuthorizableTest extends TestCase
 {
-    use PermissionOwnerTest;
+    use PermissionOwnerTest, RoleOwnerTest, OwnerTest;
 
     public function test_the_permissions_are_loaded_correctly(): void
     {
@@ -55,5 +56,50 @@ class AuthorizableTest extends TestCase
         $user->grant($permission);
         $user->revoke($permission);
         $this->cannot($user)($permission);
+    }
+
+    public function test_the_roles_are_loaded_correctly(): void
+    {
+        $user = $this->user();
+        $roles = $this->roles(3);
+        $user->syncGrant($roles->all());
+
+        $user->getRoleModels()->each(function (RoleContract $role) use ($roles) {
+            $this->assertTrue($roles->contains($this->equalsAuthorization($role)));
+        });
+    }
+
+    public function test_a_user_can_have_roles(): void
+    {
+        $user = $this->user();
+        $roles = $this->roles(3);
+        $user->syncGrant($roles->all());
+        $roles->each($this->is($user));
+    }
+
+    public function test_a_user_can_have_a_role(): void
+    {
+        $user = $this->user();
+        $role = $this->role();
+        $user->grant($role);
+        $this->is($user)($role);
+    }
+
+    public function test_you_can_remove_multiple_roles_to_a_user(): void
+    {
+        $user = $this->user();
+        $roles = $this->roles(3);
+        $user->syncGrant($roles->all());
+        $user->syncRevoke($roles->all());
+        $roles->each($this->isnt($user));
+    }
+
+    public function test_you_can_remove_single_roles_to_a_user(): void
+    {
+        $user = $this->user();
+        $role = $this->role();
+        $user->grant($role);
+        $user->revoke($role);
+        $this->isnt($user)($role);
     }
 }
