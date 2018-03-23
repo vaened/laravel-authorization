f9dd18a2a16930592245f689cf8e5d2fc4723873
enea dhack <enea.so@live.com>
2018-03-23 18:59:15 -0500

    Refactoring DeterminerTest

--- tests/DeterminerTest.php
+++ tests/DeterminerTest.php
@@ -12,19 +12,21 @@ class DeterminerTest extends TestCase
     public function test_the_default_listeners_are_active(): void
     {
         $this->app->make('config')->set('authorization', null);
-        $this->assertEquals(Determiner::listenUnauthorizedOwnerEventForLogger(), true);
+        $this->assertTrue(Determiner::listenUnauthorizedOwnerEventForLogger());
     }
 
     public function test_its_loading_custom_listeners(): void
     {
         $this->loadCustomListenerConfig();
-        $this->assertEquals(Determiner::listenUnauthorizedOwnerEventForLogger(), false);
+        $this->assertFalse(Determiner::listenUnauthorizedOwnerEventForLogger());
     }
 
     private function loadCustomListenerConfig(): void
     {
-        $this->app->make('config')->set('authorization.listeners', [
-            'unauthorized-owner-logger' => false,
+        $this->app->make('config')->set('authorization', [
+            'listeners' => [
+                'unauthorized-owner-logger' => false,
+            ],
         ]);
     }
 }
