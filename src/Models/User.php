a048f5f4788cb2666e515e07e59bcb6b0f50443c
enea dhack <enea.so@live.com>
2018-03-24 21:47:59 -0500

    Add base user model

---
+++ src/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author enea dhack <me@enea.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Enea\Authorization\Models;
+
+use Enea\Authorization\Contracts\Authorizable as AuthorizableContract;
+use Enea\Authorization\Traits\Authorizable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract, AuthorizableContract
+{
+    use Authenticatable, CanResetPassword, Authorizable;
+}
