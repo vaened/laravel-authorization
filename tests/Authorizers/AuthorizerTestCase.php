8b7c0e3a8e300b7561e41204107463c28628c895
enea dhack <enea.so@live.com>
2018-03-30 17:37:18 -0500

    test: add test for authorizers

--- tests/Authorizers/AuthorizerTestCase.php
+++ tests/Authorizers/AuthorizerTestCase.php
@@ -3,7 +3,10 @@
 declare(strict_types=1);
 
 /**
- * Created on 11/03/18 by enea dhack.
+ * @author enea dhack <me@enea.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Enea\Authorization\Tests\Authorizers;
@@ -18,6 +21,16 @@ abstract class AuthorizerTestCase extends DriverTestCase
         $user = $this->user();
         $user->grant($this->permission('See Articles'));
         $this->assertTrue($this->getAuthorizer()->can($user, 'see-articles'));
+    }
+
+    public function test_can_check_that_a_user_does_not_have_a_permission(): void
+    {
+        $user = $this->user();
+        $permission = $this->permission('See Articles');
+        $user->grant($permission);
+        $this->assertTrue($this->getAuthorizer()->can($user, 'see-articles'));
+        $user->revoke($permission);
+        $this->assertFalse($this->getAuthorizer()->can($user, 'see-articles'));
     }
 
     public function test_check_that_you_have_some_permissions(): void
@@ -46,6 +59,16 @@ abstract class AuthorizerTestCase extends DriverTestCase
         $user = $this->user();
         $user->grant($this->role('Articles Owner'));
         $this->assertTrue($this->getAuthorizer()->is($user, 'articles-owner'));
+    }
+
+    public function test_can_check_that_a_user_does_not_have_a_role(): void
+    {
+        $user = $this->user();
+        $role = $this->role('Articles Owner');
+        $user->grant($role);
+        $this->assertTrue($this->getAuthorizer()->is($user, 'articles-owner'));
+        $user->revoke($role);
+        $this->assertFalse($this->getAuthorizer()->is($user, 'articles-owner'));
     }
 
     public function test_check_that_you_have_some_role(): void
