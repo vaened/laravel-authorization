46a29f39bc53a0766680c367d572e58a7facc586
enea dhack <enea.so@live.com>
2018-03-30 17:35:56 -0500

    fix(tests): update the tests for the cache manager

--- tests/Drivers/Cache/ManagerTest.php
+++ tests/Drivers/Cache/ManagerTest.php
@@ -12,36 +12,46 @@ declare(strict_types=1);
 namespace Enea\Authorization\Tests\Drivers\Cache;
 
 use Enea\Authorization\Contracts\GrantableOwner;
-use Enea\Authorization\Contracts\PermissionsOwner;
-use Enea\Authorization\Contracts\RolesOwner;
 use Enea\Authorization\Drivers\Cache\KeyBuilder;
 use Enea\Authorization\Drivers\Cache\Manager;
 use Enea\Authorization\Drivers\Cache\Repositories\PermissionRepository;
 use Enea\Authorization\Drivers\Cache\Repositories\RoleRepository;
 use Enea\Authorization\Tests\TestCase;
 use Illuminate\Cache\Repository as CacheContract;
-use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Collection as EloquentCollection;
 use Mockery;
 use Mockery\MockInterface;
 
 class ManagerTest extends TestCase
 {
+    private $relationship;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->relationship = Mockery::mock(BelongsToMany::class);
+        $this->relationship->shouldReceive('get')->andReturn(new EloquentCollection());
+    }
+
     public function test_the_permission_cache_works_correctly(): void
     {
-        $mock = Mockery::mock(PermissionsOwner::class);
-        $mock->shouldReceive('getIdentificationKey')->times(4);
-        $mock->shouldReceive('getPermissionModels')->once()->andReturn(new EloquentCollection());
+        $mock = Mockery::mock('Enea\Authorization\Contracts\PermissionsOwner[permissions]')->shouldIgnoreMissing();
+        $mock->shouldReceive('permissions')->twice()->andReturn($this->relationship);
+        $this->manager()->permissions($mock);
         $this->manager()->permissions($mock);
         $this->verifyCache($mock, PermissionRepository::getSuffix());
+        $this->manager()->permissions($mock);
     }
 
     public function test_the_roles_cache_works_correctly(): void
     {
-        $mock = Mockery::mock(RolesOwner::class);
-        $mock->shouldReceive('getIdentificationKey')->times(4);
-        $mock->shouldReceive('getRoleModels')->once()->andReturn(new EloquentCollection());
+        $mock = Mockery::mock('Enea\Authorization\Contracts\RolesOwner[roles]')->shouldIgnoreMissing();
+        $mock->shouldReceive('roles')->twice()->andReturn($this->relationship);
+        $this->manager()->roles($mock);
         $this->manager()->roles($mock);
         $this->verifyCache($mock, RoleRepository::getSuffix());
+        $this->manager()->roles($mock);
     }
 
     private function verifyCache(MockInterface $mock, string $suffix): void
