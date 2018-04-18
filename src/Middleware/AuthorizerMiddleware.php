93ddbec640aaff2f9c6c4a3fffbaddf65b39820e
Enea Dhack <enea.so@live.com>
2018-04-18 14:10:48 -0500

    refactor(middleware): user authenticated helper

--- src/Middleware/AuthorizerMiddleware.php
+++ src/Middleware/AuthorizerMiddleware.php
@@ -12,45 +12,15 @@ declare(strict_types=1);
 namespace Enea\Authorization\Middleware;
 
 use Closure;
-use Enea\Authorization\Authorizer;
-use Enea\Authorization\Contracts\Owner;
-use Enea\Authorization\Events\UnauthorizedOwner;
-use Enea\Authorization\Exceptions\UnauthorizedOwnerException;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Events\Dispatcher;
 use Illuminate\Http\Request;
 
 abstract class AuthorizerMiddleware
 {
-    protected $authorizer;
+    abstract protected function authorized(array $grantable): void;
 
-    private $event;
-
-    public function __construct(Authorizer $authorizer, Dispatcher $event)
+    public function handle(Request $request, Closure $next, string ...$grantable)
     {
-        $this->authorizer = $authorizer;
-        $this->event = $event;
-    }
-
-    abstract protected function authorized(Owner $owner, array $grantables): bool;
-
-    public function handle(Request $request, Closure $next, string ...$grantables)
-    {
-        $authenticated = $request->user();
-
-        if ($this->isAuthorizedRequestFor($authenticated)($grantables)) {
-            return $next($request);
-        }
-
-        $this->event->dispatch(new UnauthorizedOwner($authenticated, $grantables));
-
-        throw new UnauthorizedOwnerException($authenticated);
-    }
-
-    private function isAuthorizedRequestFor(Model $authenticated): Closure
-    {
-        return function (array $grantables) use ($authenticated): bool {
-            return $authenticated instanceof Owner && $this->authorized($authenticated, $grantables);
-        };
+        $this->authorized($grantable);
+        return $next($request);
     }
 }
