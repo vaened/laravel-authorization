b8f2cbaa0b4956e7e8a485cd788a63f6385dde06
Enea Dhack <enea.so@live.com>
2018-04-18 10:50:09 -0500

    test: change the name of the determining tests

--- tests/DeterminerTest.php
+++ tests/DeterminerTest.php
@@ -3,7 +3,10 @@
 declare(strict_types=1);
 
 /**
- * Created on 18/03/18 by enea dhack.
+ * @author enea dhack <me@enea.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Enea\Authorization\Tests;
@@ -12,14 +15,14 @@ use Enea\Authorization\Support\Determiner;
 
 class DeterminerTest extends TestCase
 {
-    public function test_the_default_listeners_are_active(): void
+    public function test_the_default_config_are_active(): void
     {
         $this->app->make('config')->set('authorization', null);
         $this->assertTrue(Determiner::listenUnauthorizedOwnerEventForLogger());
         $this->assertTrue(Determiner::transformSecretNameToKebabCase());
     }
 
-    public function test_its_loading_custom_listeners(): void
+    public function test_its_loading_custom_config(): void
     {
         $this->loadCustomListenerConfig();
         $this->assertFalse(Determiner::listenUnauthorizedOwnerEventForLogger());
