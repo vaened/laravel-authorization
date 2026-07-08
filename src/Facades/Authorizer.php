6397f2b380a5cbe5bb6731c127f86076b102cf4f
Enea Dhack <enea@vaened.dev>
2026-07-08 11:44:54 -0500

    refactor: update `Authorizer` facade to restrict `can` and `cannot` methods to `Subject` type

--- src/Facades/Authorizer.php
+++ src/Facades/Authorizer.php
@@ -15,12 +15,11 @@ namespace Vaened\Authorization\Facades;
 use Illuminate\Support\Facades\Facade;
 use Vaened\Sentinel\Authorization\Authorizer as AuthorizerService;
 use Vaened\Sentinel\Authorization\Junction;
-use Vaened\Sentinel\Role;
 use Vaened\Sentinel\Subject;
 
 /**
- * @method static bool can(Subject|Role $owner, array $permissions, Junction $junction = Junction::Or)
- * @method static bool cannot(Subject|Role $owner, array $permissions, Junction $junction = Junction::Or)
+ * @method static bool can(Subject $subject, array $permissions, Junction $junction = Junction::Or)
+ * @method static bool cannot(Subject $subject, array $permissions, Junction $junction = Junction::Or)
  * @method static bool is(Subject $subject, array $roles, Junction $junction = Junction::Or)
  * @method static bool isnt(Subject $subject, array $roles, Junction $junction = Junction::Or)
  *
