a4c265fd5d7c21f8a92cdb7a310b78697439a00d
Enea Dhack <enea@vaened.dev>
2026-09-25 16:25:08 -0500

    docs: add author and license headers to core classes

--- src/Console/SyncAuthorizations.php
+++ src/Console/SyncAuthorizations.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/**
+ * @author enea dhack <contact@vaened.dev>
+ * @link https://vaened.dev DevFolio
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 namespace Vaened\Authorization\Console;
 
 use Illuminate\Console\Command;
